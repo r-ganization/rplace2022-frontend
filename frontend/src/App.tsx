79743432a64a633f104a350e0b9fabd1b02340b1
StianWiu <Pignuuu@gmail.com>
2022-04-13 14:09:15 +0200

    Design simple WIP page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { BsGithub, BsDiscord, BsReddit } from "react-icons/bs";
+
+// Import kofi widget
+const KofiButton = require("kofi-button");
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+      <h1 className="Header">rPlace 2022</h1>
+      <h3 className="Header-child"></h3>
+      <div className="About-us">
+        <p className="About-us-paragraph">
+          We are a team developing a non-profit, user-friendly interface for the
+          r/place dataset visualization and imagery generation.
+          <br />
+          <br />
+          This website is still a work in progress, consider joining the Discord
+          or the Reddit for updates.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      </div>
+      <a href="https://github.com/r-ganization">r-ganization</a>
+      <br />
+      <KofiButton
+        color="#15A2DF"
+        title="Support the team"
+        kofiID="rganization"
+      />
+      <div className="footer-link-icons">
+        <a href="https://discord.gg/rcpZbN5f">
+          <BsDiscord size={40} className="footer-icon" title="Discord" />
         </a>
-      </header>
+        <a href="https://github.com/r-ganization">
+          <BsGithub size={40} className="footer-icon" title="Github" />
+        </a>
+        <a href="https://www.reddit.com/r/ganization/">
+          <BsReddit size={40} className="footer-icon" title="Reddit" />
+        </a>
+      </div>
     </div>
   );
 }
